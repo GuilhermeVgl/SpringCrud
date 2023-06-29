008a90bae9022e295ab3307a2c8c2d431fd27d36
Guilherme Vieira <guilhermeharfy@gmail.com>
2023-06-28 22:41:59 -0400

    :Style: componente table e array de objetos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Course } from "../model/course.model";
 
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  selector: "app-courses",
+  templateUrl: "./courses.component.html",
+  styleUrls: ["./courses.component.scss"]
 })
 export class CoursesComponent {
 
+  courses: Course[] = [
+    {courseid: 1, name: "Curso de Java", category: "Back-end Software Development", price: 45},
+    {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development", price: 95},
+    {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development", price: 50},
+    {courseid: 1, name: "Curso de React", category: "Front-end Software Development", price: 15},
+  ];
+
+  visibleColumns = ["name", "category", "price"];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
 }
---
+++ src/app/courses/model/course.model.ts
@@ -0,0 +1,6 @@
+export interface Course {
+  courseid: number;
+  name: string;
+  category: string;
+  price: number;
+}
