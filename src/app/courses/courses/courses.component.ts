5ec146e3cb869babd7eea84690e0c357c657d795
Guilherme Vieira <guilhermeharfy@gmail.com>
2023-06-30 19:46:44 -0400

    :Feat: incluido data nmo array de objetos de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -9,19 +9,17 @@ import { Course } from "../model/course.model";
 export class CoursesComponent {
 
   courses: Course[] = [
-    {courseid: 1, name: "Curso de Java", category: "Back-end Software Development", price: 45},
-    {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development", price: 95},
-    {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development", price: 50},
-    {courseid: 1, name: "Curso de React", category: "Front-end Software Development", price: 15},
+    {courseid: 1, name: "Curso de Java", category: "Back-end Software Development", price: 45, dataLancamento: new Date(2023, 6, 24)},
+    {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development", price: 95, dataLancamento: new Date(2023, 6, 24)},
+    {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development", price: 50, dataLancamento: new Date(2023, 6, 24)},
+    {courseid: 1, name: "Curso de React", category: "Front-end Software Development", price: 15, dataLancamento: new Date(2023, 6, 24)},
   ];
 
-  visibleColumns = ["name", "category", "price"];
+  visibleColumns = ["name", "category", "price", "dataLancamento"];
 
   constructor() {
   }
 
   ngOnInit(): void {
-
-    
   }
 }
--- src/app/courses/model/course.model.ts
+++ src/app/courses/model/course.model.ts
@@ -3,4 +3,5 @@ export interface Course {
   name: string;
   category: string;
   price: number;
+  dataLancamento: Date;
 }
