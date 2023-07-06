87b449812eafbed53206c2792a3a5f9a19824f2f
Guilherme Vieira <guilhermeharfy@gmail.com>
2023-07-05 23:51:50 -0400

    :Feat: A

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -9,13 +9,13 @@ import { Course } from "../model/course.model";
 export class CoursesComponent {
 
   courses: Course[] = [
-    {courseid: 1, name: "Curso de Java", category: "Back-end Software Development"},
-    {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development"},
-    {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development"},
-    {courseid: 1, name: "Curso de React", category: "Front-end Software Development"},
+    {courseid: 1, name: "Curso de Java", category: "Back-end Software Development", price: 45},
+    {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development", price: 95},
+    {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development", price: 65},
+    {courseid: 1, name: "Curso de React", category: "Front-end Software Development", price: 55},
   ];
 
-  visibleColumns = ["name", "category",];
+  visibleColumns = ["name", "category", "price"];
 
   constructor() {
   }
