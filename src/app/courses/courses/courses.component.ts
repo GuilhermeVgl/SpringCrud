9337af8717433665900fe1c899f6aaa623e9f2da
Guilherme Vieira <guilhermeharfy@gmail.com>
2023-07-06 22:13:23 -0400

    :None:

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from "@angular/core";
 import { Course } from "../model/course.model";
 
@@ -6,18 +7,15 @@ import { Course } from "../model/course.model";
   templateUrl: "./courses.component.html",
   styleUrls: ["./courses.component.scss"]
 })
-export class CoursesComponent {
+export class CoursesComponent  implements OnInit{
 
-  courses: Course[] = [
-    {courseid: 1, name: "Curso de Java", category: "Back-end Software Development", price: 45},
-    {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development", price: 95},
-    {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development", price: 65},
-    {courseid: 1, name: "Curso de React", category: "Front-end Software Development", price: 55},
-  ];
-
+  courses: Course[] = [];
   visibleColumns = ["name", "category", "price"];
+  CoursesService: CoursesService
 
   constructor() {
+    this.CoursesService = new CoursesService();
+    this.courses = this.CoursesService.list();
   }
 
   ngOnInit(): void {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { Course } from '../model/course.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
   constructor() { }
+
+  list(): CoursesService {
+    return [
+      {courseid: 1, name: "Curso de Java", category: "Back-end Software Development", price: 45},
+      {courseid: 1, name: "Curso de Angular", category: "Front-end Software Development", price: 95},
+      {courseid: 1, name: "Curso de Spring Boot", category: "Back-end Software Development", price: 65},
+      {courseid: 1, name: "Curso de React", category: "Front-end Software Development", price: 55},
+    ]
+  }
 }
